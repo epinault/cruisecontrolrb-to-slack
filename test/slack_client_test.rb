66ef040e9ccb0f8ac20a1b42f382f87d19cfabd8
epinault <emmanuel.pinault@gmail.com>
2015-04-03 21:45:02 -0700

    Fix broken tests

--- test/slack_client_test.rb
+++ test/slack_client_test.rb
@@ -1,19 +1,17 @@
 require 'test_helper'
+require 'pry'
 
 describe CruisecontrolrbToSlack::SlackClient do
-  subject{CruisecontrolrbToSlack::SlackClient.new('team', 'token')}
+  subject{CruisecontrolrbToSlack::SlackClient.new('http://url.to/webhook')}
 
   it 'should use the default  a new instance' do
     subject.client.must_be_kind_of Slack::Notifier
   end 
 
   it 'should set the team properly' do
-    subject.client.team.must_equal 'team'
+    subject.client.endpoint.to_s.must_equal 'http://url.to/webhook'
   end
 
-  it 'should set the token properly' do
-    subject.client.token.must_equal 'token'
-  end
 
   it 'should set the default channel to #general' do
     subject.client.channel.must_equal '#general'
@@ -24,12 +22,12 @@ describe CruisecontrolrbToSlack::SlackClient do
   end
 
   it 'should override the default channel ' do
-    slack_client = CruisecontrolrbToSlack::SlackClient.new('team', 'token', channel: 'channel')
+    slack_client = CruisecontrolrbToSlack::SlackClient.new('webhook', channel: 'channel')
     slack_client.client.channel.must_equal 'channel'
   end
 
   it 'should override the default user ' do
-    slack_client = CruisecontrolrbToSlack::SlackClient.new('team', 'token', user: 'user')
+    slack_client = CruisecontrolrbToSlack::SlackClient.new('webhook', user: 'user')
     slack_client.client.default_payload[:username].must_equal 'user'
   end
 end
